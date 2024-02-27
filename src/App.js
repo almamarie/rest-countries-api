ab8484e6d4ef0f7e20a6f09bf043ae20f322911e
Louis Marie Atoluko Ayariga <alouismariea97@gmail.com>
2024-02-27 23:12:50 +0000

    Successfully added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
+import Country from "./pages/country/Country";
+import MainError from "./pages/error/MainError";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <MainError />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "countries/",
+          element: <Home />,
+        },
+        { path: "countries/:countryFullName", element: <Country /> },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/error/MainError.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainError = () => {
+  return (
+    <div>
+      Error loading page <br /> Refresh page and if error persists, check your
+      internet
+    </div>
+  );
+};
+
+export default MainError;
